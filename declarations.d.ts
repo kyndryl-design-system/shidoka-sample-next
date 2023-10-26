6afbdd3554d52975a52def09720a4c1533fdcc68
Brian Patrick <brian.patrick3@kyndryl.com>
2023-10-25 20:50:42 -0500

    initial commit

---
+++ declarations.d.ts
@@ -0,0 +1,19 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'kyn-ui-shell': any;
+    'kyn-header': any;
+    'kyn-header-nav': any;
+    'kyn-header-link': any;
+    'kyn-header-flyouts': any;
+    'kyn-header-flyout': any;
+    'kyn-header-panel': any;
+    'kyn-header-panel-link': any;
+    'kyn-header-avatar': any;
+    'kyn-local-nav': any;
+    'kyn-local-nav-link': any;
+    'kyn-footer': any;
+    'kyn-footer-nav': any;
+    'kyn-footer-nav-link': any;
+    'kd-chart': any;
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,82 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import reactifyWc from 'reactify-wc';
+import '@kyndryl-design-system/shidoka-charts/components/chart';
+
+const KdChart = reactifyWc('kd-chart');
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="kd-grid">
+        <div className="kd-grid__col--sm-4 kd-grid__col--md-8 kd-grid__col--lg-12">
+          <h1 className="kd-type--headline-03">Shidoka Next Sample App</h1>
+        </div>
+
+        <div className="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-8">
+          <KdChart
+            type="bar"
+            height="400"
+            chartTitle="Bar Chart"
+            description="Description"
+            labels={['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']}
+            datasets={[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+              {
+                label: 'Dataset 2',
+                data: [8, 15, 7, 9, 6, 12],
+              },
+            ]}
+            options={{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
+                  },
+                },
+                y: {
+                  title: {
+                    text: 'Votes',
+                  },
+                },
+              },
+            }}
+          />
+        </div>
+
+        <div className="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-4">
+          <KdChart
+            type="doughnut"
+            chartTitle="Doughnut Chart"
+            description="Description"
+            height="400"
+            labels={['Blue', 'Red', 'Orange', 'Yellow', 'Green', 'Purple']}
+            datasets={[
+              {
+                label: 'Dataset 1',
+                data: [12, 19, 3, 5, 2, 3],
+              },
+            ]}
+            options={{
+              scales: {
+                x: {
+                  title: {
+                    text: 'Color',
+                  },
+                },
+                y: {
+                  title: {
+                    text: 'Votes',
+                  },
+                },
+              },
+            }}
+          />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
